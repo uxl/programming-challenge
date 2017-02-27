d08a6d0cc2b830dda2f3b381b16213ca6697046d
brian.wilkinson <brian@ulx.io>
2017-02-26 22:19:43 -0500

    - add SoundEffects class - create instance in index.js for SoundEffects - Set to play startup sound

---
+++ src/SoundEffects.ts
@@ -0,0 +1,24 @@
+// import Howl = require("howler");
+import { Howl } from "howler";
+
+export class SoundEffects {
+  public sndsprite:Howl;
+    constructor() {
+      //load sprite
+      this.sndsprite = new Howl({
+      src: ['src/audio/sprite.wav'],
+      sprite: {
+        start: [0, 3000],
+        dot: [4000, 1000],
+        line: [6000, 5000],
+        typing: [6000, 5000],
+        error: [6000, 5000],
+        move: [6000, 5000]
+      }
+    });
+    }
+    public play = function(snd:string):void{
+      console.log("snd", snd);
+      this.sndsprite.play(snd);
+    }
+  }
