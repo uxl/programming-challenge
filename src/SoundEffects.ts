5958a48697b992d7691a12845374eac18d9b142d
Brian Wilkinson <brian@ulx.io>
2017-03-06 16:01:09 -0500

    audio config
    - separate sound files for each sound
    - remove sound sprite

--- src/SoundEffects.ts
+++ src/SoundEffects.ts
@@ -1,22 +1,46 @@
 import { Howl } from "howler";
 
 export class SoundEffects {
-    public sndsprite: Howl;
+    // public sndsprite: Howl;
+    private soundObj: any = {};
     constructor() {
-        //load sprite
-        this.sndsprite = new Howl({
-            src: ['src/audio/sprite.wav'],
-            sprite: {
-                keypress: [10, 54],
-                start: [249, 1650],
-                klaxon: [2000, 2100],
-                move: [0, 1],
-                yelp: [0, 1]
-            }
+        //load sounds
+        var keypress = new Howl({
+            src: ['src/audio/keypress.wav'],
+            preload: true
         });
+
+        var beep = new Howl({
+            src: ['src/audio/beep.wav'],
+            preload: true
+        });
+
+        var start = new Howl({
+            src: ['src/audio/startup.wav'],
+            preload: true
+        });
+
+        var result = new Howl({
+            src: ['src/audio/nope.wav'],
+            preload: true
+        });
+
+        var yelp = new Howl({
+            src: ['src/audio/yelp.wav'],
+            preload: true
+        });
+
+        this.soundObj['keypress'] = keypress;
+        this.soundObj['beep'] = beep;
+        this.soundObj['start'] = start;
+        this.soundObj['result'] = result;
+        this.soundObj['yelp'] = yelp;
+        console.log(this.soundObj);
     }
     public play = function(snd: string): void {
         //console.log("snd", snd);
-        this.sndsprite.play(snd);
+        // this.sndsprite.play(snd);
+        //console.log("play: " + snd);
+        this.soundObj[snd].play();
     }
 }
